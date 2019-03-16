6e8d2227a950c378a06eadcef0893aa5804a8949
Alexandra Middleton <alexandra.middleton@blackrock.com>
2019-03-16 16:59:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function takeANumber(currentLine, newperson) {
+var katzDeliLine = [];
+
+function takeANumber(current, newperson) {
   
   
-  return "Welcome," + personname + ". You are number "
+  
+  return "Welcome, " + personname + ". You are number " + personnumber + " in line.";
+  
+}
+
+function nowServing(current) {
+  
+  
+}
+
+function currentLine(current) {
+  
 }
