044ee114082a0d981bc7ab72251282940739286d
Alexandra Middleton <alexandra.middleton@blackrock.com>
2019-03-16 18:37:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-var katzDeliLine = [];
-
 function takeANumber(current, newperson) {
   var personnumber;
   current.push(newperson);
   personnumber = current.indexOf(newperson) + 1;
-  
+
   return "Welcome, " + newperson + ". You are number " + personnumber + " in line.";
-  
 }
 
 function nowServing(current) {
-  
   var oldline = current.slice();
   current.shift();
   
@@ -26,7 +22,7 @@ function currentLine(current) {
   if (current.lenth===0) {
     console.log("The line is currently empty.")
   } else {
-    for(n=0; n<current.length; n++) { 
+    for(var n = 0; n < current.length; n++) { 
       numberedline[n] = (n+1) + "." + current[n];
     }
   }
