387181b15b70d335355ae9f762570dbd38dc2bab
Alexandra Middleton <alexandra.middleton@blackrock.com>
2019-03-16 17:55:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,4 +23,10 @@ function nowServing(current) {
 
 function currentLine(current) {
   
+  if (current.lenth===0) {
+    console.log("The line is currently empty.")
+  } else {
+    console.log("The line is currently:")
+    for(n=0; n<current.length(); n++) {
+    
 }
